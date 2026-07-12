ebedac524cba467a0c9fc79d8ecdb255fdf16157
vardhan-vvs <velivelavardhan@gmail.com>
2026-07-12 19:50:09 -0400

    added extra metrics

--- le_net.py
+++ le_net.py
@@ -5,6 +5,13 @@ from tensorflow.keras.layers import Input, Conv2D, AveragePooling2D, Flatten, De
 from tensorflow.keras.datasets import mnist
 from tensorflow.keras.utils import to_categorical
 import numpy as np
+from sklearn.metrics import (accuracy_score,
+                             confusion_matrix,
+                             classification_report,
+                             precision_score,
+                             recall_score,
+                             f1_score,
+                             roc_auc_score)
 
 class LeNet:
     def __init__(self, batch_size=32, epochs=20):
@@ -122,3 +129,35 @@ class LeNet:
         # predictions = np.argmax(self.loaded_model.predict(images), axis=1)
         predictions = self.loaded_model.predict(images)
         return predictions
+    
+    def evaluate(self, x_test, y_test):
+        '''
+        This function will compute metrics []
+        It will take two parameters, test images and their labels
+        It will return a dictionary of metrics
+        '''
+        y_pred = self.loaded_model.predict(x_test)
+        y_pred_labels = np.argmax(y_pred, axis=1)
+        
+        accuracy = accuracy_score(y_test, y_pred_labels)
+        cm = confusion_matrix(y_test, y_pred_labels)
+        classReport = classification_report(y_test, y_pred_labels)
+        precision = precision_score(y_test, y_pred_labels, average='macro')
+        recall = recall_score(y_test, y_pred_labels, average='macro')
+        f1 = f1_score(y_test, y_pred_labels, average='macro')
+        
+        y_test_onehot = to_categorical(y_test, 10)
+        auc = roc_auc_score(y_test_onehot, y_pred, multi_class='ovr', average='macro')
+        
+        metrics = {
+            'accuracy': accuracy,
+            'precision': precision,
+            'recall': recall,
+            'f1 score':f1,
+            'roc and auc': auc
+        }
+        
+        print("Confusion matrix:\n", cm)
+        print("Classification Report:\n", classReport)
+        
+        return metrics
--- module8.py
+++ module8.py
@@ -9,15 +9,18 @@ from tensorflow.keras.datasets import mnist
 
 if __name__ == "__main__":
     # Parse command-line arguments
-    parser = argparse.ArgumentParser()
-    parser.add_argument("image_filename", type=str)
-    parser.add_argument("actual_digit", type=int)
-    args = parser.parse_args()
+    # parser = argparse.ArgumentParser()
+    # parser.add_argument("image_filename", type=str)
+    # parser.add_argument("actual_digit", type=int)
+    # args = parser.parse_args()
     
     # file path of our model
     model_path_name = 'lenet_model.keras'
     # to test our model
     (_, _), (x_test, y_test) = mnist.load_data()
+    
+    x_test_prep = x_test/255.0
+    x_test_prep = x_test_prep.reshape(x_test_prep.shape[0],28,28,1)
     
     # Initialize and load model
     network = LeNet(batch_size=64, epochs=20)
@@ -30,24 +33,29 @@ if __name__ == "__main__":
     network.load(model_path_name)
     
     
-    grayscale_image = Image.open(args.image_filename).convert('L')
-    resized_image = grayscale_image.resize((28, 28))  # Resize to 28x28
-    image_data = np.array(resized_image).astype(np.float32)
-    image_data = image_data.flatten() / 255.0  # Normalize
+    # grayscale_image = Image.open(args.image_filename).convert('L')
+    # resized_image = grayscale_image.resize((28, 28))  # Resize to 28x28
+    # image_data = np.array(resized_image).astype(np.float32)
+    # image_data = image_data.flatten() / 255.0  # Normalize
     
-    # y_hat = network.predict(x_test[0:16])
-    image_data = image_data.reshape(1, 28,28)
-    y_hat = network.predict(image_data)
+    y_hat = network.predict(x_test_prep)
+    y_hat_labels = np.argmax(y_hat, axis = 1)
+    # image_data = image_data.reshape(1, 28,28)
+    # y_hat = network.predict(image_data)
     
-    y_hat = np.argmax(y_hat)
-    # print(y_test[0:16])
+    # y_hat = np.argmax(y_hat)
+    metrics = network.evaluate(x_test_prep, y_test)
+    print(metrics)
     
-    image = plt.imread(args.image_filename)
-    plt.imshow(image, cmap="gray")
-    plt.show()
+    # image = plt.imread(args.image_filename)
+    # plt.imshow(image, cmap="gray")
+    # plt.show()
     
     # Output result
-    if y_hat == args.actual_digit:
-        print(f"Success: Image {args.image_filename} is for digit {args.actual_digit} is recognized as {y_hat}.")
-    else:
-        print(f"Fail: Image {args.image_filename} is for digit {args.actual_digit} but the inference result is {y_hat}.")
+    # if y_hat == args.actual_digit:
+        # print(f"Success: Image {args.image_filename} is for digit {args.actual_digit} is recognized as {y_hat}.")
+    # else:
+    #     print(f"Fail: Image {args.image_filename} is for digit {args.actual_digit} but the inference result is {y_hat}.")
+    
+
+    
