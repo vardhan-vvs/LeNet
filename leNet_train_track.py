a056598d247578148492acffdc3bfe6d0c578382
vardhan-vvs <velivelavardhan@gmail.com>
2026-07-16 22:09:51 -0400

    tracking almost done

--- leNet_train_track.py
+++ leNet_train_track.py
@@ -3,7 +3,7 @@ from le_net import LeNet
 import numpy as np
 from PIL import Image
 import os
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 from tensorflow.keras.datasets import mnist
 import mlflow
 
@@ -19,15 +19,17 @@ if __name__ == "__main__":
     x_test_prep = x_test/255.0
     x_test_prep = x_test_prep.reshape(x_test_prep.shape[0],28,28,1)
     
+    batchs_epochs = [[32,20],[64,10]]#,[64,40],[128,20]]
     mlflow.tensorflow.autolog()
     
-    with mlflow.start_run(run_name='LeNet_') as run:
+    with mlflow.start_run(run_name='LeNet_hyperparameters_search') as parent_run:
         # Initialize and load model
-        network = LeNet(batch_size=64, epochs=20)
-        if os.path.exists(f'{model_path_name}'):
-            pass
-        else:
-            network.train()
+        for b_e in batchs_epochs:
+            with mlflow.start_run(run_name = f'trail_b{b_e[0]}_e{b_e[1]}', nested = True) as child_run:
+                mlflow.log_param("batch size", b_e[0])
+                mlflow.log_param("epochs", b_e[1])
+                network = LeNet(batch_size=b_e[0], epochs=b_e[1])
+                network.train()
     
         # metrics = network.evaluate(x_test_prep, y_test)
         # predictions = network.predict()
--- save_best.py
+++ save_best.py
@@ -1,1 +1,9 @@
 ## this file will fetch the best model, register it and save it locally
+
+import mlflow
+
+experiment_id = mlflow.get_experiment_by_name()
+
+best_run_df = mlflow.search_runs(
+    experiment_ids = [experiment_id]
+)
